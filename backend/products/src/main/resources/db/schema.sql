1c7947879ad814873c3cde62e4b630c5116fa88e
Ernst Robert <rernst2002@gmail.com>
2025-05-31 20:22:47 +0300

    create the stores microservice

--- backend/products/src/main/resources/db/schema.sql
+++ backend/products/src/main/resources/db/schema.sql
@@ -8,6 +8,7 @@ CREATE TABLE products (
                           category VARCHAR(255) NOT NULL,
                           sizes TEXT[],
                           price DOUBLE PRECISION NOT NULL,
+                          old_price DOUBLE PRECISION,
                           description TEXT,
                           reviews DOUBLE PRECISION DEFAULT 0,
                           stock INT DEFAULT 0
